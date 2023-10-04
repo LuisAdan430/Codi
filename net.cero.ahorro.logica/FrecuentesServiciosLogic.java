1aec03df374bf637032e3f4d650d3f3cead4d7f7
gibranfsait <gibran.fuentes@soyait.mx>
2023-10-04 10:10:35 -0600

    Controllers

--- net.cero.ahorro.logica/FrecuentesServiciosLogic.java
+++ net.cero.ahorro.logica/FrecuentesServiciosLogic.java
@@ -1,5 +1,36 @@
 package net.cero.ahorro.logica;
+import net.cero.data.FrecuentesConsulta;
+import net.cero.data.FrecuentesDesactivar;
+import net.cero.data.FrecuentesOBJ;
+import net.cero.data.Respuesta;
 
 public class FrecuentesServiciosLogic {
-
+	public Boolean validarInputRegistrarFrecuente(FrecuentesOBJ data) {
+		return true;
+	}
+	
+	public Respuesta registrarFrecuente(FrecuentesOBJ data) {
+		Respuesta respuesta = new Respuesta();
+		return respuesta;
+	}
+	
+	public Boolean validarInputConsulta(String data) {
+		return true;
+	}
+	public FrecuentesConsulta consultarFrecuentes(String tipo) {
+		FrecuentesConsulta consulta = new FrecuentesConsulta();
+		/*
+		 
+		 * */
+		return consulta;
+	}
+	
+	public Boolean validarInputDesactivarFrecuente(FrecuentesDesactivar data) {
+		return true;
+	}
+	
+	public Respuesta desactivarFrecuente (FrecuentesDesactivar data) {
+		Respuesta respuesta = new Respuesta();
+		return respuesta;
+	}
 }
--- net.cero.ahorro.ws/FrecuentesServicios.java
+++ net.cero.ahorro.ws/FrecuentesServicios.java
@@ -1,46 +1,160 @@
 package net.cero.ahorro.ws;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 import net.cero.spring.config.IPAuthenticationProvider;
+import net.cero.data.FrecuentesConsulta;
+import net.cero.data.FrecuentesDesactivar;
+import net.cero.data.FrecuentesOBJ;
+import net.cero.data.Respuesta;
+import net.cero.ahorro.logica.FrecuentesServiciosLogic;
 
 @Controller
 public class FrecuentesServicios {
 	@Autowired
 	protected IPAuthenticationProvider authenticationManager;
-	private static final Logger log = LogManager.getLogger(DesactivarFavorito.class);
-	@PostMapping(path = "/insertarFrecuente", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public ResponseEntity<String> insertarFrecuente(@RequestBody String json){
-		log.info("---->Insertar Frecuente<----");
-		return new ResponseEntity<>(HttpStatus.OK); 
+
+	private static final Logger log = LogManager.getLogger(FrecuentesWS.class);
+	FrecuentesServiciosLogic frec = new FrecuentesServiciosLogic();
+
+	@RequestMapping(value = "/registrarFavortio", method = RequestMethod.POST)
+	public ResponseEntity<String> registrarFavortio(@RequestBody String json) {
+		SecurityContext securityContext = SecurityContextHolder.getContext();
+		Authentication authenticate;
+		ResponseEntity<String> response;
+		Gson gson = new Gson();
+		Respuesta resp = new Respuesta();
+		response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+		authenticate = authenticationManager.authenticate(securityContext.getAuthentication());
+
+		if (!authenticate.isAuthenticated()) {
+			response = new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+			return response;
+		}
+
+		try {
+			FrecuentesOBJ input = gson.fromJson(json, FrecuentesOBJ.class);
+
+			if (!frec.validarInputRegistrarFrecuente(input)) {
+				resp.setCodigo(2);
+				resp.setMensaje("Parametro inválido ");
+				response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.BAD_REQUEST);
+			} else {
+				resp = frec.registrarFrecuente(input);
+				if (resp.getCodigo() == -1)
+					response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+				else
+					response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.OK);
+			}
+		} catch (JsonSyntaxException j) {
+			j.printStackTrace();
+			if (j.getMessage().contains("NumberFormatException")) {
+				resp.setCodigo(3);
+				resp.setMensaje("Error de syntaxis, formato de entrada incorrecto");
+				response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.BAD_REQUEST);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return response;
 	}
-	@PostMapping(path = "/consultarFrecuenteTerceros", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public ResponseEntity<String> consultarFrecuenteTerceros(@RequestBody String json){
-		log.info("---->Consultar Frecuente Terceros<----");
-		return new ResponseEntity<>(HttpStatus.OK); 
+
+	@RequestMapping(value = "/consultarFrecuentes", method = RequestMethod.POST)
+	public ResponseEntity<String> consultarFrecuentesDeRecargas(@RequestBody String json) {
+		SecurityContext securityContext = SecurityContextHolder.getContext();
+		Authentication authenticate;
+		ResponseEntity<String> response;
+		Gson gson = new Gson();
+		FrecuentesConsulta resp = new FrecuentesConsulta();
+		response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+		authenticate = authenticationManager.authenticate(securityContext.getAuthentication());
+
+		if (!authenticate.isAuthenticated()) {
+			response = new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+			return response;
+		}
+
+		try {
+
+			if (!frec.validarInputConsulta(json)) {
+				resp.setCodigo(2);
+				resp.setMensaje("Parametro inválido ");
+				response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.BAD_REQUEST);
+			} else {
+				resp = frec.consultarFrecuentes(json);
+				if (resp.getCodigo() == -1)
+					response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+				else
+					response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.OK);
+			}
+		} catch (JsonSyntaxException j) {
+			j.printStackTrace();
+			if (j.getMessage().contains("NumberFormatException")) {
+				resp.setCodigo(3);
+				resp.setMensaje("Error de syntaxis, formato de entrada incorrecto");
+				response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.BAD_REQUEST);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return response;
 	}
-	@PostMapping(path = "/consultarFrecuenteRecargas", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public ResponseEntity<String> consultarFrecuenteRecargas(@RequestBody String json){
-		log.info("---->Consultar Frecuente Recargas<----");
-		return new ResponseEntity<>(HttpStatus.OK); 
-	}
-	@PostMapping(path = "/desactivarFrecuente", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public ResponseEntity<String> desactivarFrecuente(@RequestBody String json){
-		log.info("---->Desactivar Frecuente<----");
-		return new ResponseEntity<>(HttpStatus.OK); 
+
+	@RequestMapping(value = "/desactivarFrecuente", method = RequestMethod.POST)
+	public ResponseEntity<String> desactivarFrecuente(@RequestBody String json) {
+		SecurityContext securityContext = SecurityContextHolder.getContext();
+		Authentication authenticate;
+		ResponseEntity<String> response;
+		Gson gson = new Gson();
+		Respuesta resp = new Respuesta();
+		response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+		authenticate = authenticationManager.authenticate(securityContext.getAuthentication());
+
+		if (!authenticate.isAuthenticated()) {
+			response = new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+			return response;
+		}
+		try {
+			FrecuentesDesactivar input = gson.fromJson(json, FrecuentesDesactivar.class);
+
+			if (!frec.validarInputDesactivarFrecuente(input)) {
+				resp.setCodigo(2);
+				resp.setMensaje("Parametro inválido ");
+				response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.BAD_REQUEST);
+			} else {
+				resp = frec.desactivarFrecuente(input);
+				if (resp.getCodigo() == -1)
+					response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+				else
+					response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.OK);
+			}
+		} catch (JsonSyntaxException j) {
+			j.printStackTrace();
+			if (j.getMessage().contains("NumberFormatException")) {
+				resp.setCodigo(3);
+				resp.setMensaje("Error de syntaxis, formato de entrada incorrecto");
+				response = new ResponseEntity<>((gson.toJson(resp)).toString(), HttpStatus.BAD_REQUEST);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return response;
 	}
 	
 }
